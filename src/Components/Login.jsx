563a374ebcedb48baec3b98b46b91baf0ffeb0ca
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-13 14:34:20 +0530

    complete

---
+++ src/Components/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [data, changeData] = useState(
+        [
+            {
+                "email":"",
+                "password":""
+            }
+        ]
+    )
+    const inputHandler=(event)=>{
+        changeData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">email</label>
+                                <input type="email" className="form-control" name='email' value={data.email} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">password</label>
+                                <input type="password" className="form-control" name='password' value={data.password} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-success" onClick={readValue}>Sign in</button>
+                                <center><a href="/">sign up</a></center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
